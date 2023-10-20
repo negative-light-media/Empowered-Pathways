d4f70e8b1b82f371af57cec8d77a2bd4d744d4ec
Negative_light <53065989+Negative-light@users.noreply.github.com>
2023-10-20 13:52:52 -0400

    Blocks are now added from old mod. Works both forge and fabric

---
+++ common/src/main/java/com/negativelight/empoweredpathways/block/custom/StoneworkPath.java
@@ -0,0 +1,11 @@
+package com.negativelight.empoweredpathways.block.custom;
+
+import net.minecraft.world.level.block.DirtPathBlock;
+
+public class StoneworkPath  extends DirtPathBlock {
+    public StoneworkPath(Properties $$0) {
+        super($$0);
+    }
+
+    public static void loadClass() {}
+}
