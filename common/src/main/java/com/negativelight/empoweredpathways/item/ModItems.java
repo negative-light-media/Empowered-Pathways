182c87c502ed0de2ef8b2d89635bbcfe4315f3d0
Negative_light <53065989+Negative-light@users.noreply.github.com>
2023-10-21 16:29:12 -0400

    Added Grader Item. Now Tills blocks. **TODO: Add Texture, Lang and Crafting**

--- common/src/main/java/com/negativelight/empoweredpathways/item/ModItems.java
+++ common/src/main/java/com/negativelight/empoweredpathways/item/ModItems.java
@@ -1,6 +1,24 @@
 package com.negativelight.empoweredpathways.item;
 
+import com.negativelight.empoweredpathways.Constants;
+import com.negativelight.empoweredpathways.item.custom.GraderItem;
+import com.negativelight.empoweredpathways.registration.RegistryObject;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tiers;
+
+import java.util.function.Supplier;
+
+import static com.negativelight.empoweredpathways.Constants.ITEMS;
+
 public class ModItems {
 
+    public static RegistryObject<Item> GRADER_TOOL = registerItem("grader",
+            () -> new GraderItem(Tiers.IRON, 1.5F, -3.0F, new Item.Properties()));
     public static void loadClass() {}
+
+    public static <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> itemSupplier) {
+        Constants.LOG.info("REGISTERING BLOCK AND ITEM " + name);
+        //Items.DIRT
+        return ITEMS.register(name, itemSupplier);
+    }
 }
