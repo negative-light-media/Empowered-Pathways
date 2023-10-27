e684c83059ad5c3ec505fdcd436c9472d22d5f42
Negative_light <53065989+Negative-light@users.noreply.github.com>
2023-10-27 14:31:45 -0400

    Finial Commit before 1.0 Release (#8)
    * Added Github Workflows closes #3
    
    * Created basic outline for README.md. Helps with #1.
    
    * Added Items to Forge Inventories. Closes #6
    
    * Added Items to Fabric Inventory. Closes #7
    
    * Found a Bug where the Infused Stonework Block Recipe was incorrect
    
    * Corrected Infused recipe again. Complted Readme in #1
    
    * corrected spelling errors
    
    * Added Comments to provide context on crafting recipies

--- fabric/src/main/java/com/negativelight/empoweredpathways/EmpoweredPathways.java
+++ fabric/src/main/java/com/negativelight/empoweredpathways/EmpoweredPathways.java
@@ -1,6 +1,11 @@
 package com.negativelight.empoweredpathways;
 
+import com.negativelight.empoweredpathways.block.ModBlocks;
+import com.negativelight.empoweredpathways.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class EmpoweredPathways implements ModInitializer {
     
@@ -16,5 +21,20 @@ public class EmpoweredPathways implements ModInitializer {
         CommonClass.init();
 
 
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(EmpoweredPathways::addBuildingBlocks);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(EmpoweredPathways::addTools);
     }
+
+    private static void addBuildingBlocks(FabricItemGroupEntries entries) {
+        entries.accept(ModBlocks.STONEWORK_BLOCK.get());
+        entries.accept(ModBlocks.INFUSED_STONEWORK_BLOCK.get());
+        entries.accept(ModBlocks.REINFORCED_STONEWORK_BLOCK.get());
+    }
+
+    private static void addTools(FabricItemGroupEntries entries) {
+        entries.accept(ModItems.GRADER_TOOL.get());
+    }
+
+
+
 }
--- forge/src/main/java/com/negativelight/empoweredpathways/EmpoweredPathways.java
+++ forge/src/main/java/com/negativelight/empoweredpathways/EmpoweredPathways.java
@@ -1,9 +1,13 @@
 package com.negativelight.empoweredpathways;
 
+import com.negativelight.empoweredpathways.block.ModBlocks;
+import com.negativelight.empoweredpathways.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class EmpoweredPathways {
@@ -13,19 +17,25 @@ public class EmpoweredPathways {
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
-    
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Use Forge to bootstrap the Common mod.
         Constants.LOG.info("Hello Forge world!");
         CommonClass.init();
 
-        MinecraftForge.EVENT_BUS.addListener(this::buildContents);
+        eventBus.addListener(this::buildContents);
         //MinecraftForge.EVENT_BUS.addListener(this::onItemTooltip);
     }
 
     public void buildContents(BuildCreativeModeTabContentsEvent event) {
-        Constants.LOG.info("Adding to Creative Mode Tab " + event.getTabKey());
+        //Constants.LOG.info("Adding to Creative Mode Tab " + event.getTabKey());
         if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            //event.accept(ModBlocks.STONEWORK_BLOCK);
+            event.accept(ModBlocks.STONEWORK_BLOCK);
+            event.accept(ModBlocks.INFUSED_STONEWORK_BLOCK);
+            event.accept(ModBlocks.REINFORCED_STONEWORK_BLOCK);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.GRADER_TOOL);
         }
     }
 
